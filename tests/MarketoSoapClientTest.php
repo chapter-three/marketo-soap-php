76e47427e1c7012e24d075d5c284b8faad7c5e43
Jaesin Mulenex <jaesin@no-reply.gradientsky.com>
2016-07-29 14:10:29 -0700

    [CI] Test travis environment variables.

--- tests/MarketoSoapClientTest.php
+++ tests/MarketoSoapClientTest.php
@@ -14,8 +14,18 @@ class MarketoSoapClientTest extends \PHPUnit_Framework_TestCase {
    * @todo: The constructor should have everything it needs to connect.
    */
   public function testConstructor() {
-    $client = new MarketoSoapClient('bigcorp2_458073844B29ACAFC64AC0', '425794457179585644BB2299AACCBB01CC66229C2B35', 'https://example.mktoapi.com/soap/mktows/2_9');
+    // Get API credentials from environment variables.
+    $id = getenv('marketo_api_id');
+    $secret = getenv('marketo_api_secret');
+    $uri = getenv('marketo_api_uri');
 
+
+    $this->assertEquals('bigcorp2_458073844B29ACAFC64AC0', $id);
+    $this->assertEquals('425794457179585644BB2299AACCBB01CC66229C2B35', $secret);
+    $this->assertEquals('https://example.mktoapi.com/soap/mktows/2_9', $uri);
+
+
+    $client = new MarketoSoapClient($id, $secret, $uri);
     $this->assertEquals('http://www.marketo.com/mktows/', $client::MKTOWS_NAMESPACE);
   }
 }
